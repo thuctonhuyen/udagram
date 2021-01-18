a16a6c09ce291d9e8eeae027dff885c71a2422ce
Thuc Huyen <thuctonhuyen@gmail.com>
2021-01-17 19:54:37 -0800

    cleanup

--- src/controllers/filtered_image/routes/filtered_image.router.ts
+++ src/controllers/filtered_image/routes/filtered_image.router.ts
@@ -4,29 +4,28 @@ import isUrl from 'is-url';
 
 import {filterImageFromURL, deleteLocalFiles} from '../../../util/util';
 
-
 const router: Router = Router();
 
 // Get filtered image by imageUrl
 router.get('/', async(req: Request, res: Response) => {
   try {
-    const imageUrl = _.get(req, 'query.image_url');
+    const imageUrl:string = _.get(req, 'query.image_url');
 
     if (!isUrl(imageUrl)) {
       return res.status(400).send({ message: 'Valid URL is required.' });
     }
 
     const filteredImageUrl:string = await filterImageFromURL(imageUrl);
-    if(filterImageFromURL) {
+    if (filterImageFromURL) {
       res.status(201).sendFile(filteredImageUrl);
       res.on('finish', () => {
         deleteLocalFiles([filteredImageUrl]);
       })
     } else {
-      return res.status(400).send({ message: 'Valid URL is required or malformed.' });
+      return res.status(400).send({ message: 'Cannot process the image.' });
     }
   } catch(err) {
-    return res.status(400).send({ message: 'Valid URL is required or malformed.' });
+    return res.status(400).send({ message: 'Cannot process the image.' });
   }
 })
 
