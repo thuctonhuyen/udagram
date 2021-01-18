71f4f13caa07fa11d1475decf65733e0f7ac7528
Thuc Huyen <thuctonhuyen@gmail.com>
2021-01-17 18:43:41 -0800

    feat: implement fitlered_image router to handle request to filter image to grayscale

--- src/controllers/filtered_image/routes/filtered_image.router.ts
+++ src/controllers/filtered_image/routes/filtered_image.router.ts
@@ -1,31 +1,33 @@
 import { Router, Request, Response } from 'express';
 import _ from 'lodash';
+import isUrl from 'is-url';
+
+import {filterImageFromURL, deleteLocalFiles} from '../../../util/util';
+
 
 const router: Router = Router();
 
 // Get filtered image by imageUrl
-router.get('/', async(req, res) => {
-  const imageUrl = _.get(req, 'query.image_url');
+router.get('/', async(req: Request, res: Response) => {
+  try {
+    const imageUrl = _.get(req, 'query.image_url');
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+    if (!isUrl(imageUrl)) {
+      return res.status(400).send({ message: 'Valid URL is required.' });
+    }
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-
-  console.log('image url', imageUrl);
+    const filteredImageUrl:string = await filterImageFromURL(imageUrl);
+    if(filterImageFromURL) {
+      res.status(201).sendFile(filteredImageUrl);
+      res.on('finish', () => {
+        deleteLocalFiles([filteredImageUrl]);
+      })
+    } else {
+      return res.status(400).send({ message: 'Valid URL is required or malformed.' });
+    }
+  } catch(err) {
+    return res.status(400).send({ message: 'Valid URL is required or malformed.' });
+  }
 })
 
 export const FilteredImageRouter: Router = router;
